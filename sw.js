c5453df7061bdc0d39bc414e72b28eb0a2823f66
Idin Rohidin <54070368+idinrohidin@users.noreply.github.com>
2019-11-11 12:22:03 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,45 @@
+---
+---
 
+self.addEventListener('install', function(e) {  
+  e.waitUntil(caches.open('blog-{{ site.github.build_revision }}').then(function(cache) {
+    return cache.addAll([
+      {% for page in site.pages %}
+      {%- if page.url != '/sw.js' -%} 
+      '{{ page.url | remove: '.html' }}',
+      {%- endif -%}
+      {% endfor %}
+      {% for post in site.posts %}      
+      '{{ post.url }}',     
+      {% endfor %}
+      {% for file in site.static_files %}       
+      '{{ site.baseurl }}{{ file.path }}',    
+      {% endfor %}                
+      'https://cdn.statically.io/gh/idinrohidin/css/2cb6275d/idinrohidin.min.css',
+      'https://cdn.statically.io/gh/idinrohidin/js/ce50b484/jquery-full.min.js',
+      'https://cdn.statically.io/gh/idinrohidin/js/0ff197cf/bootstrap.min.js',
+      'https://cdn.statically.io/gh/idinrohidin/js/2238b70f/jquery.easing.min.js',
+      'https://cdn.statically.io/gh/idinrohidin/js/60e9a49d/modernrecent.min.js',
+      'https://cdn.statically.io/gh/idinrohidin/js/0ef138a8/creativy.min.js',      
+      'https://cdn.statically.io/gh/idinrohidin/js/2fa14abd/idinrohidin.min.js',      
+    ]);
+  }));
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(cacheNames) {
+    return Promise.all(
+      cacheNames.map(function(cacheName) {
+        if (cacheName != 'blog-{{ site.github.build_revision }}') {
+          return caches.delete(cacheName);
+        }
+      })
+    );
+  }));
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(caches.match(e.request).then(function(response) {   
+    return response || fetch(e.request);
+  }));
+});
